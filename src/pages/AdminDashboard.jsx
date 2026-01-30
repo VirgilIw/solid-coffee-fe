77114c1ce5d4f8d2d2466023a3787cf21fc9e656
Rahman <fadhllulrahman687@gmail.com>
2026-01-31 01:51:41 +0700

    feat:continue admin dashboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import DashboardMenu from '../components/adminDashboard/AdminDashboardMenu'
 import UserlistMenu from '../components/adminDashboard/UserlistMenu'
 import AdminDashboardMenu from '../components/adminDashboard/AdminDashboardMenu'
+import ProductChart from '../components/adminDashboard/ProductChart'
 
 function AdminDashboard() {
   return (
-    <div className="mt-8 px-5 py-10 lg:px-10 grid grid-cols-[30%_70%]">
+    <div className="mt-8 px-5 py-10 lg:px-10 grid grid-cols-[300px_670px] lg:grid-cols-[20%_80%]">
       <AdminDashboardMenu/>
-      <UserlistMenu/>
+      <ProductChart/>
     </div>
   )
 }
